e2e573faabde91f3e3608c3d957ed365ff061f2f
Mickaël Lambert <mickael.lambert@wildcodeschool.com>
2024-11-05 12:05:47 +0100

    🛠️- Fix main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,26 @@
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App.tsx'
+import Jobs from './pages/Jobs.tsx'
 import Job from './pages/Job.tsx'
+import './index.css'
+
+const router = createBrowserRouter([
+    {
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <Jobs />
+            },
             {
                 path: "/jobs/:id",
                 element: <Job />
             }
+        ]
+    }
+])
+
+createRoot(document.getElementById('root')!).render(
+    <RouterProvider router={router} />
+)
